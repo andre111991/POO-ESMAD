637c36f6999cbc55883af156ba58d3cc5fcd18ae
andre111991 <andre.sousa.pinheiro@gmail.com>
2025-03-21 16:46:05 +0000

    ficha 3 finalizada

--- FICHA 3/index.js
+++ FICHA 3/index.js
@@ -1,28 +1,57 @@
-//let nome_produto = prompt("Indique o nome do produto:");
-//let cat_produto = prompt("Indique a categoria do produto:");
+//1 
+
+let nome_produto = prompt("Indique o nome do produto:");
+let cat_produto = prompt("Indique a categoria do produto:");
+
 
 const compra = { 
-    nome_produto: 'Banana', 
-    cat_produto: 'Fruta',
+    nome_produto, 
+    cat_produto,
 }
-
+// 3 e 4 
 compra.preco_base = "2.10"   // Adicionar uma propriedade fora do objeto
 compra.tx_IVA = "0.23"
 
+
+//5
 compra.preco_base = "2.50"  // Alterar o valor de uma propriedade
 
 console.log(compra)
 
+
+//6
 compra.preco_final=function(){
     return this.preco_final = this.preco_base * this.tx_IVA;
 }
 
 console.log(compra.preco_final())  // Mostrar o valor final do produto
 
- 
+ //7
 compra.dados_encomenda = {              // Adicionar um objeto dentro de um objeto
     data_encomenda : "2023-03-27",
     prazo_entrega : '(7) dias',
     local_entrega : 'Avenida do Mar, 74, 4460-810 Matosinhos'
 } 
 
+//8
+function showLength(obj) {
+    return Object.keys(obj).length // object.keys() retorna todas as propriedades do objeto dentro dos ()
+}
+
+console.log(`Número de propriedades do objeto compra é ${showLength(compra)}`)  // Mostrar o número de propriedades do objeto compra
+
+//9
+function showProperties(obj){
+    return Object.keys(obj)
+}
+
+console.log(`As propriedades do objeto compra é ${showProperties(compra)}`)
+
+//10
+function removeProp(arg, prop){
+    delete arg[prop]
+}
+
+removeProp(compra,`tx_IVA`)
+
+console.log(compra)
