4169a3ff63976801156c869718d27ab5274e082a
andre111991 <andre.sousa.pinheiro@gmail.com>
2025-03-21 17:02:49 +0000

    ficha 4 a meio

--- FICHA 04/index.js
+++ FICHA 04/index.js
@@ -14,35 +14,39 @@ function listarNivel(nivel) {
     let lista_nivel = listUsers.filter(element => element.nivel == nivel);
     let texto = `Users no nível ${nivel}:`
     for(let element of lista_nivel) {
-        texto += `
-        ${jogador.nome}`;
+        texto += `${element.nome}`;
     }
-    alerta(texto);
+    alert(texto);
+    console.log(texto)
 
 }
 
-//listarNivel(1);
+listarNivel(nivel);
 
 //2
+
 
 function somarPontos(nivel) {
 
     let lista_nivel = listUsers.filter(element => element.nivel == nivel);
     let total_pontos = lista_nivel.reduce((sum,element) => sum + element.pontos, 0); // reduce() é um método que reduz o array a um único valor. 0 é o valor inicial do acumulador
-    alerta(`Total de pontos no nivel ${nivel}: ${total_pontos}`);
+    alert(`Total de pontos no nivel ${nivel}: ${total_pontos}`);
+    console.log(`Total de pontos no nivel ${nivel}: ${total_pontos}`)
 }
-//somarPontos2(1);
+somarPontos(nivel);
 
 //3
-
 function verificarNivel(nivel){
     console.log(`Nivel ${nivel}:`, listUsers.some(element => element.nivel == nivel)); //some() verifica se algum elemento do array satisfaz a condição
 
 }
+
+verificarNivel(nivel)
 
 //4
 function pesquisarColecionaveis(colecionavel) {
     console.log(`Colecionavel ${objetoCol}:`,listUsers.some(element => element.colecionaveis.includes(objetoCol))); //includes() verifica se um array contém um determinado elemento
 }
 
+//pesquisarColecionaveis(colecionaveis)
 //5
